99a7702a856bbe60f5a27f5a44a882fdde4ead60
iPablol <106445718+iPablol@users.noreply.github.com>
2025-04-02 19:34:20 +0200

    Loop exit

--- HTTPClient/Program.cs
+++ HTTPClient/Program.cs
@@ -9,25 +9,33 @@ namespace HTTPClient
         static void Main(string[] args)
         {
             Test();
-            while (true)
+            string command = "";
+            while (command != "quit")
             {
-
+                command = Console.ReadLine() ?? "";
             }
         }
 
         static async void Test()
         {
-            var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 83);
+            try
+            {
+                var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 83);
 
-            using TcpClient client = new();
-            await client.ConnectAsync(ipEndPoint);
-            await using NetworkStream stream = client.GetStream();
+                using TcpClient client = new();
+                await client.ConnectAsync(ipEndPoint);
+                await using NetworkStream stream = client.GetStream();
 
-            var buffer = new byte[1_024];
-            int received = await stream.ReadAsync(buffer);
+                var buffer = new byte[1_024];
+                int received = await stream.ReadAsync(buffer);
 
-            var message = Encoding.UTF8.GetString(buffer, 0, received);
-            Console.WriteLine($"Message received: \"{message}\"");
+                var message = Encoding.UTF8.GetString(buffer, 0, received);
+                Console.WriteLine($"Message received: \"{message}\"");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
