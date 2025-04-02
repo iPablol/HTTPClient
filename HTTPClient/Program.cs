988e62f9e59317815c9f4fc8cf873187626e95d4
iPablol <106445718+iPablol@users.noreply.github.com>
2025-04-02 19:28:55 +0200

    First commit

---
+++ HTTPClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace HTTPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            while (true)
+            {
+
+            }
+        }
+
+        static async void Test()
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 83);
+
+            using TcpClient client = new();
+            await client.ConnectAsync(ipEndPoint);
+            await using NetworkStream stream = client.GetStream();
+
+            var buffer = new byte[1_024];
+            int received = await stream.ReadAsync(buffer);
+
+            var message = Encoding.UTF8.GetString(buffer, 0, received);
+            Console.WriteLine($"Message received: \"{message}\"");
+
+        }
+    }
+}
